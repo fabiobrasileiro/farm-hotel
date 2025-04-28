f7343115cf482eef7207efbab4dcfabcb3008baf
Fábio Brasileiro <brasileirowinner@gmail.com>
2025-04-28 19:12:53 -0300

    feature: create page maintenance and route

---
+++ client/src/app/maintenance/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../(auth)/layout/layout'
+
+function Maintenance() {
+  return (
+    <Layout>
+      <h1>Maintenance Mode</h1>
+      <p>We are currently undergoing maintenance. Please check back later.</p>
+    </Layout>
+  )
+}
+
+export default Maintenance
