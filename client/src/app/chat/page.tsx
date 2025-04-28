909f712663922f25a33ac6229abf9f9ccd9a6a40
Fábio Brasileiro <brasileirowinner@gmail.com>
2025-04-28 19:11:37 -0300

    feature: create page chat and route

---
+++ client/src/app/chat/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../(auth)/layout/layout'
+
+function Chat() {
+  return (
+    <Layout>
+      <h1 className="text-2xl font-bold">Chat Page</h1>
+      <p className="mt-4">This is where you can chat with others.</p>
+    </Layout>
+
+
+  )
+}
+
+export default Chat
