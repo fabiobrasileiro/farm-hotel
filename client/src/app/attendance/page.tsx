fd0c9a73d016c5721bab1b5b2133475cf0d94e75
Fábio Brasileiro <brasileirowinner@gmail.com>
2025-04-28 19:10:01 -0300

    feature: create page attendance and route

---
+++ client/src/app/attendance/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../(auth)/layout/layout'
+
+function Attendance() {
+  return (
+    <Layout>
+      <div>
+        Attendance
+      </div>
+    </Layout>
+  )
+}
+
+export default Attendance
